005023df3f28b295b181683975bfbf46b8e638de
robmarkcole <robmarkcole@gmail.com>
2018-07-10 07:09:40 +0100

    Adds test_process_image

--- tests/test_classificationbox.py
+++ tests/test_classificationbox.py
@@ -78,3 +78,47 @@ async def test_setup_platform(hass):
         await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
         await hass.async_block_till_done()
         assert hass.states.get(VALID_ENTITY_ID)
+
+
+async def test_process_image(hass, mock_image):
+    """Test processing of an image."""
+    with patch('homeassistant.components.image_processing.classificationbox.get_models',
+               return_value=MOCK_MODELS):
+        await async_setup_component(hass, ip.DOMAIN, VALID_CONFIG)
+        await hass.async_block_till_done()
+        assert hass.states.get(VALID_ENTITY_ID)
+
+    classification_events = []
+
+    @callback
+    def mock_classification_event(event):
+        """Mock event."""
+        classification_events.append(event)
+
+    hass.bus.async_listen('image_processing.image_classification',
+                          mock_classification_event)
+
+    with requests_mock.Mocker() as mock_req:
+        url = 'http://{}:{}/{}/models/{}/predict'.format(
+            MOCK_IP,
+            MOCK_PORT,
+            cb.CLASSIFIER,
+            MOCK_MODEL_ID)
+        mock_req.post(url, json=MOCK_JSON)
+        data = {ATTR_ENTITY_ID: VALID_ENTITY_ID}
+        await hass.services.async_call(ip.DOMAIN,
+                                       ip.SERVICE_SCAN,
+                                       service_data=data)
+        await hass.async_block_till_done()
+
+    state = hass.states.get(VALID_ENTITY_ID)
+    assert state.state == 'birds'
+    assert state.attributes.get(ip.ATTR_CONFIDENCE) == ip.DEFAULT_CONFIDENCE
+
+    assert state.attributes.get(cb.ATTR_MODEL_ID) == MOCK_MODEL_ID
+    assert (state.attributes.get(CONF_FRIENDLY_NAME) ==
+            'classificationbox demo_camera 12345')
+
+    assert len(classification_events) == 1
+    assert classification_events[0].data[ATTR_ID] == 'birds'
+    assert classification_events[0].data[ip.ATTR_CONFIDENCE] == 91.59
